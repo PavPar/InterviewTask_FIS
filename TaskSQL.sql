9708733f8e62bee94b3805c0d1f84fc75f42f473
PavPar <ParamonPav@yandex.ru>
2021-09-03 12:41:06 +0300

    feat: init

---
+++ TaskSQL.sql
@@ -0,0 +1,53 @@
+﻿-- SELECT BookID,MAX(pop) as most,BookInfo.AuthorID,BookInfo.FirstName,BookInfo.LastName,BookInfo.MiddleName
+-- FROM(
+--   SELECT BookID, COUNT(DISTINCT StudentID) AS pop
+--   FROM handout 
+--   WHERE YEAR(TakenAT)=2021
+--   GROUP BY BookID
+--  ) AS PopBooks
+-- JOIN 
+--   (
+--   SELECT b.id,b.AuthorID,a.FirstName,a.LastName,a.MiddleName
+--   FROM book as b JOIN author as a ON b.AuthorID = a.AuthorID 
+--   ) AS BookInfo
+--  ON PopBooks.BookID=BookInfo.id;
+
+SET @Year = 2019;
+
+SELECT Distinct BookInfo.AuthorID,BookInfo.FirstName,BookInfo.LastName,BookInfo.MiddleName
+FROM(
+SELECT A.BookID
+FROM (  
+  SELECT BookID, COUNT(DISTINCT StudentID) AS pop
+  FROM handout AS A
+  WHERE YEAR(TakenAT)=@Year
+  GROUP BY BookID 
+) AS A LEFT OUTER JOIN ( 
+  SELECT BookID, COUNT(DISTINCT StudentID) AS pop
+  FROM handout 
+  WHERE YEAR(TakenAT)=@Year
+  GROUP BY BookID
+) AS B
+on A.BookID != B.BookID and A.pop>B.pop
+WHERE b.BookID is NULL ) AS PopBooks
+JOIN 
+  (
+  SELECT b.id,b.AuthorID,a.FirstName,a.LastName,a.MiddleName
+  FROM book as b JOIN author as a ON b.AuthorID = a.AuthorID 
+  ) AS BookInfo
+ ON PopBooks.BookID=BookInfo.id;
+-- 
+-- SELECT A.BookID
+-- FROM (  
+--   SELECT BookID, COUNT(DISTINCT StudentID) AS pop
+--   FROM handout AS A
+--   WHERE YEAR(TakenAT)=2021
+--   GROUP BY BookID 
+-- ) AS A LEFT OUTER JOIN ( 
+--   SELECT BookID, COUNT(DISTINCT StudentID) AS pop
+--   FROM handout 
+--   WHERE YEAR(TakenAT)=2021
+--   GROUP BY BookID
+-- ) AS B
+-- on A.BookID != B.BookID and A.pop>B.pop
+-- WHERE b.BookID is NULL
