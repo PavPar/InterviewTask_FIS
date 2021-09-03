a615506702772ab9d52ecdba36d66bfcc23f4ea7
PavPar <paramonpav@yandex.ru>
2021-09-03 13:53:33 +0300

    feat: fixes

---
+++ task_2.sql
@@ -0,0 +1,24 @@
+﻿SET @Year = 2019;
+
+SELECT Distinct BookInfo.AuthorID,BookInfo.FirstName,BookInfo.LastName,BookInfo.MiddleName
+FROM(
+SELECT A.BookID
+FROM (  
+  SELECT BookID, COUNT(DISTINCT StudentID) AS pop
+  FROM handout AS A
+  WHERE YEAR(TakenAT)=@Year
+  GROUP BY BookID 
+) AS A LEFT OUTER JOIN ( 
+  SELECT BookID, COUNT(DISTINCT StudentID) AS pop
+  FROM handout 
+  WHERE YEAR(TakenAT)=@Year
+  GROUP BY BookID
+) AS B
+on A.BookID != B.BookID and A.pop>B.pop
+WHERE b.BookID is NULL ) AS PopBooks
+JOIN 
+  (
+  SELECT b.id,b.AuthorID,a.FirstName,a.LastName,a.MiddleName
+  FROM book as b JOIN author as a ON b.AuthorID = a.AuthorID 
+  ) AS BookInfo
+ ON PopBooks.BookID=BookInfo.id;
